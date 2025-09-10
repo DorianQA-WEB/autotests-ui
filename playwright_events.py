04a613ce0487beddd3e62a71dbb0354a7448c0f9
DorianQA-WEB <qa.doriangrey@gmail.com>
2025-09-10 18:58:17 +0500

    Введение в Playwright

---
+++ playwright_events.py
@@ -0,0 +1,12 @@
+from playwright.sync_api import sync_playwright
+
+
+with sync_playwright() as playwright:
+    chromium = playwright.chromium.launch(headless=False)
+    page = chromium.new_page()
+
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
+
+
+    page.on('request')
+    page.on('response')
