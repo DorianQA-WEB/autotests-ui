629bdecae730e5dfc0e6392134bc8af9d7724a4b
DorianQA-WEB <qa.doriangrey@gmail.com>
2025-09-21 15:00:09 +0500

    Знакомство с PageComponent

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -13,24 +13,6 @@ def test_empty_courses_list(chromium_page_with_state, courses_list_page: Courses
         courses_list_page.check_visible_create_course_button()
         courses_list_page.check_empty_view_icon()
 
-
-        # chromium_page_with_state.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
-        #
-        # title_courses = chromium_page_with_state.get_by_test_id('courses-list-toolbar-title-text')
-        # expect(title_courses).to_be_visible()
-        # expect(title_courses).to_have_text('Courses')
-        #
-        # there_is_not_result = chromium_page_with_state.get_by_test_id('courses-list-empty-view-title-text')
-        # expect(there_is_not_result).to_be_visible()
-        # expect(there_is_not_result).to_have_text('There is no results')
-        #
-        # icon = chromium_page_with_state.get_by_test_id('courses-list-empty-view-icon')
-        # expect(icon).to_be_visible()
-        #
-        # result_from_the_load = chromium_page_with_state.get_by_test_id('courses-list-empty-view-description-text')
-        # expect(result_from_the_load).to_be_visible()
-        # expect(result_from_the_load).to_have_text('Results from the load test pipeline will be displayed here')
-
 @pytest.mark.courses
 @pytest.mark.regression
 def test_create_course(courses_list_page: CoursesListPage, create_course_page: CreateCoursePage):
