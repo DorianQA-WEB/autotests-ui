5f16ca15ee8f8134e4ec3d599eb344f339382769
DorianQA-WEB <qa.doriangrey@gmail.com>
2025-10-13 10:35:09 +0500

    Наращивание тестовой базы и рефакторинг

--- tests/dasboard/test_dashboard.py
+++ tests/dasboard/test_dashboard.py
@@ -1,4 +1,4 @@
-from pages.dashboard_page import DashboardPage
+from pages.dasboard.dashboard_page import DashboardPage
 import pytest
 
 @pytest.mark.dashboard
@@ -8,10 +8,10 @@ def test_dashboard_displaying(dashboard_page_with_state: DashboardPage):
     dashboard_page_with_state.sidebar.check_visible()
     dashboard_page_with_state.navbar.check_visible('username')
     dashboard_page_with_state.dashboard_toolbar_view.check_visible()
-    dashboard_page_with_state.activities_chart.check_visible()
-    dashboard_page_with_state.students_chart.check_visible()
-    dashboard_page_with_state.courses_chart.check_visible()
-    dashboard_page_with_state.scores_chart.check_visible()
+    dashboard_page_with_state.activities_chart.check_visible("Activities")
+    dashboard_page_with_state.students_chart.check_visible('Students')
+    dashboard_page_with_state.courses_chart.check_visible('Courses')
+    dashboard_page_with_state.scores_chart.check_visible('Scores')
 
 
 
