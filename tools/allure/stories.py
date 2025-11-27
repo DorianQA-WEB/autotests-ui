1f6233720b2326b33f8f98e01f42b231e28400ab
DorianQA-WEB <DorianQA-WEB@users.noreply.github.com>
2025-11-27 17:49:33 +0000

    deploy: c820d7f24c4fd682153c5d6f665adec79908c21a

--- tools/allure/stories.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class AllureStories(str, Enum):
-    COURSES = 'Curses'
-    AUTHORIZATION = 'Authorization'
-    DASHBOARD = 'Dashboard'
-    REGISTRATION = 'Registration'
